70aa7b03f4fa21eab76588dc97d7824a9d58410a
Ruidy <rnemausat@newstore.com>
2022-01-21 16:50:41 -0400

    chore: lint

--- passgen/constants.go
+++ passgen/constants.go
@@ -3,7 +3,7 @@ package passgen
 const (
 	// LOWERCASE characters if latin alphabet
 	LOWERCASE string = "abcdefghijklmnopqrstuvwxyz"
-	//UPPERCASE characters if latin alphabet
+	// UPPERCASE characters if latin alphabet
 	UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	// NUMBERS arabic
 	NUMBERS = "0123456789"
